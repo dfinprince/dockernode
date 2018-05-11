6dbc6bda83f74e15a7bce0566ed1011b67aecf1c
dfscoprince <prince.cheruvathur@dfsco.com>
2018-05-11 07:05:53 -0700

    Added pug views

--- src/js/server.js
+++ src/js/server.js
@@ -1,5 +1,10 @@
 'use strict'
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Router
+var index = require('./routes/index');
 
 // constants
 const port = process.env.port || 8080;
@@ -7,6 +12,16 @@ const host = '0.0.0.0';
 
 // Create app
 const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/users', index);
+
 app.get('/', (req,res) => {
     let s = JSON.stringify(process.env, null, "\n");
     res.send('Hello World : ' + s);
